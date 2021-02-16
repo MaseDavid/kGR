2877df6867835afbe0fd07ce5dd31487def5bdba
MaseDavid <masengutor@gmail.com>
2021-02-16 10:25:41 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// $(function() {
+//     $(window.scroll( function(){
+//         if($(this).scrollTop() >50 ) {
+//             $(".navbar") .addclass("solid bg-dark");
+//         }
+    
+//     else {
+// $(".navbar") .removeclass("solid bg-dark")
+//         }
+//     });
+// });
+
+// $(".dropdown-item").on(".click", function(e){
+//     if (this.hash !==""){
+//         e.preventDefault();
+//         let anchor = this.hash; //#projectanchor
+
+//         $('html, body').animate({
+//             scrollTop: $(anchor).offset().top
+//         },800), function(){
+//             window.location.hash= anchor
+//         }),
+//     }
+// })
